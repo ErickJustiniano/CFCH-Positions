153056a1ec217834989fdaf6589f6249f1e1f88a
ErickJustiniano <justi007_5@hotmail.com>
2015-07-08 01:24:43 -0500

    Tabla femenil y Layout

---
+++ controllers/tables_controller.js
@@ -0,0 +1,4 @@
+//GET /quizes/tables/femenil
+exports.femenil = function(req, res) {
+	res.render('tables/femenil');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var tablesController = require('../controllers/tables_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CFCH - Positions' });
 });
+
+router.get('/tables/femenil', tablesController.femenil);
 
 module.exports = router;
